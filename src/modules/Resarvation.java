7df61e64b0f176cef09530b00343974a9f1d8c6e
youssef tadlaoui <usftadlaoui@gmail.com>
2023-09-04 15:07:48 +0100

    reservation

--- src/modules/Resarvation.java
+++ src/modules/Resarvation.java
@@ -1,4 +1,38 @@
 package modules;
+import java.util.Date;
 
 public class Resarvation {
+    private Date dateDemprunt;
+    private Date datePrevueDeRetoure;
+    private Date dateDeRetoure;
+    public enum StatusLivraison {
+        Disponible,
+        Emprunté,
+        perdu
+
+    }
+
+    public Date getDateDemprunt() {
+        return dateDemprunt;
+    }
+
+    public void setDateDemprunt(Date dateDemprunt) {
+        this.dateDemprunt = dateDemprunt;
+    }
+
+    public Date getDatePrevueDeRetoure() {
+        return datePrevueDeRetoure;
+    }
+
+    public void setDatePrevueDeRetoure(Date datePrevueDeRetoure) {
+        this.datePrevueDeRetoure = datePrevueDeRetoure;
+    }
+
+    public Date getDateDeRetoure() {
+        return dateDeRetoure;
+    }
+
+    public void setDateDeRetoure(Date dateDeRetoure) {
+        this.dateDeRetoure = dateDeRetoure;
+    }
 }
